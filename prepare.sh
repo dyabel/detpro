5aa6322d09c47ce835beef2f6f5602f6b93958a4
dythu <abelazady@foxmail.com>
2022-04-24 10:24:58 +0800

    update

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,6 @@
 
 #save the embeddings of precomputed proposals encoded by the clip image encoder for reuse，this will take a long time.
-CUDA_VISIBLE_DEVICES=6,7 ./tools/dist_train.sh  configs/lvis/detpro_ens_20e.py 2 --work-dir workdirs/collect_data --cfg-options model.roi_head.load_feature=False totol_epochs=1
+CUDA_VISIBLE_DEVICES=6,7 ./tools/dist_train.sh  configs/lvis/detpro_ens_20e.py 2 --work-dir workdirs/collect_data --cfg-options model.roi_head.load_feature=False total_epochs=1
 #compress the embeddings into a zip file
 zip -r data/lvis_clip_image_embedding.zip data/lvis_clip_image_embedding/*
 #For training the DetPro, we need to save the embeddings of proposals embedded by the clip image encoder, correponding labels and ious with gt boxes. 
